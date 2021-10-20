81247828eb122b9261ef9b92c826719ecc86ce91
Shiroeuinn <vendalex01@gmail.com>
2021-10-20 19:43:36 +0200

    Script pour la double hache terminé

--- src/main/java/net/mcreator/testblocs/item/PlatiniumHoeItem.java
+++ src/main/java/net/mcreator/testblocs/item/PlatiniumHoeItem.java
@@ -3,14 +3,22 @@ package net.mcreator.testblocs.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
-import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
-import net.minecraft.item.IItemTier;
-import net.minecraft.item.HoeItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
 
 import net.mcreator.testblocs.TestBlocsModElements;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.ImmutableMultimap;
 
 @TestBlocsModElements.ModElement.Tag
 public class PlatiniumHoeItem extends TestBlocsModElements.ModElement {
@@ -22,31 +30,48 @@ public class PlatiniumHoeItem extends TestBlocsModElements.ModElement {
 
 	@Override
 	public void initElements() {
-		elements.items.add(() -> new HoeItem(new IItemTier() {
-			public int getMaxUses() {
-				return 250;
-			}
-
-			public float getEfficiency() {
-				return 6f;
-			}
-
-			public float getAttackDamage() {
-				return 0f;
-			}
-
-			public int getHarvestLevel() {
-				return 2;
-			}
-
-			public int getEnchantability() {
-				return 14;
-			}
-
-			public Ingredient getRepairMaterial() {
-				return Ingredient.fromStacks(new ItemStack());
-			}
-		}, 0, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		elements.items.add(() -> new ItemToolCustom() {
 		}.setRegistryName("platinium_hoe"));
+	}
+	private static class ItemToolCustom extends Item {
+		protected ItemToolCustom() {
+			super(new Item.Properties().group(ItemGroup.TOOLS).maxDamage(250));
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack itemstack, BlockState blockstate) {
+			return 1;
+		}
+
+		@Override
+		public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+			stack.damageItem(1, entityLiving, i -> i.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+			return true;
+		}
+
+		@Override
+		public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+			stack.damageItem(2, attacker, i -> i.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+			return true;
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 14;
+		}
+
+		@Override
+		public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
+			if (equipmentSlot == EquipmentSlotType.MAINHAND) {
+				ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+				builder.putAll(super.getAttributeModifiers(equipmentSlot));
+				builder.put(Attributes.ATTACK_DAMAGE,
+						new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", 0f, AttributeModifier.Operation.ADDITION));
+				builder.put(Attributes.ATTACK_SPEED,
+						new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", -3, AttributeModifier.Operation.ADDITION));
+				return builder.build();
+			}
+			return super.getAttributeModifiers(equipmentSlot);
+		}
 	}
 }
--- src/main/java/net/mcreator/testblocs/procedures/HacheQuandLeBlocEstDetruitAvecLoutilProcedure.java
+++ src/main/java/net/mcreator/testblocs/procedures/HacheQuandLeBlocEstDetruitAvecLoutilProcedure.java
@@ -1,14 +1,27 @@
 package net.mcreator.testblocs.procedures;
 
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.GameType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Hand;
 import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.client.network.play.NetworkPlayerInfo;
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.block.Block;
 
 import net.mcreator.testblocs.TestBlocsMod;
 
+import java.util.Random;
 import java.util.Map;
 
 public class HacheQuandLeBlocEstDetruitAvecLoutilProcedure {
@@ -49,6 +62,9 @@ public class HacheQuandLeBlocEstDetruitAvecLoutilProcedure {
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
+		double posX = 0;
+		double posY = 0;
+		double posZ = 0;
 		if ((((itemstack).getItem() == ((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem())
 				&& ((((world.getBlockState(new BlockPos((int) x, (int) y, (int) z)))
 						.getMaterial() == net.minecraft.block.material.Material.WOOD) == (true))
@@ -60,6 +76,76 @@ public class HacheQuandLeBlocEstDetruitAvecLoutilProcedure {
 												.getMaterial() == net.minecraft.block.material.Material.BAMBOO) == (true))))))) {
 			if (entity instanceof LivingEntity) {
 				((LivingEntity) entity).swing(Hand.MAIN_HAND, true);
+			}
+			posX = (double) (x - 1);
+			posY = (double) (x - 1);
+			posZ = (double) (x - 1);
+			for (int index0 = 0; index0 < (int) (3); index0++) {
+				for (int index1 = 0; index1 < (int) (3); index1++) {
+					for (int index2 = 0; index2 < (int) (3); index2++) {
+						if ((((itemstack).getOrCreateTag().getBoolean("stopMining")) == (false))) {
+							if (((((world.getBlockState(new BlockPos((int) posX, (int) posY, (int) posZ)))
+									.getMaterial() == net.minecraft.block.material.Material.WOOD) == (true))
+									|| ((((world.getBlockState(new BlockPos((int) posX, (int) posY, (int) posZ)))
+											.getMaterial() == net.minecraft.block.material.Material.GOURD) == (true))
+											|| ((((world.getBlockState(new BlockPos((int) posX, (int) posY, (int) posZ)))
+													.getMaterial() == net.minecraft.block.material.Material.BAMBOO) == (true))
+													|| (((world.getBlockState(new BlockPos((int) posX, (int) posY, (int) posZ)))
+															.getMaterial() == net.minecraft.block.material.Material.BAMBOO_SAPLING) == (true)))))) {
+								if (world instanceof World) {
+									Block.spawnDrops(world.getBlockState(new BlockPos((int) posX, (int) posY, (int) posZ)), (World) world,
+											new BlockPos((int) posX, (int) posY, (int) posZ));
+									world.destroyBlock(new BlockPos((int) posX, (int) posY, (int) posZ), false);
+								}
+								if (((new Object() {
+									public boolean checkGamemode(Entity _ent) {
+										if (_ent instanceof ServerPlayerEntity) {
+											return ((ServerPlayerEntity) _ent).interactionManager.getGameType() == GameType.CREATIVE;
+										} else if (_ent instanceof PlayerEntity && _ent.world.isRemote()) {
+											NetworkPlayerInfo _npi = Minecraft.getInstance().getConnection()
+													.getPlayerInfo(((AbstractClientPlayerEntity) _ent).getGameProfile().getId());
+											return _npi != null && _npi.getGameType() == GameType.CREATIVE;
+										}
+										return false;
+									}
+								}.checkGamemode(entity)) == (false))) {
+									{
+										ItemStack _ist = (itemstack);
+										if (_ist.attemptDamageItem((int) 1, new Random(), null)) {
+											_ist.shrink(1);
+											_ist.setDamage(0);
+										}
+									}
+									if (((((itemstack)).getDamage()) == 0)) {
+										if (world instanceof World && !world.isRemote()) {
+											((World) world).playSound(null, new BlockPos((int) posX, (int) posY, (int) posZ),
+													(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+															.getValue(new ResourceLocation("ambient.cave")),
+													SoundCategory.AMBIENT, (float) 1, (float) 0.9);
+										} else {
+											((World) world).playSound(posX, posY, posZ,
+													(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+															.getValue(new ResourceLocation("ambient.cave")),
+													SoundCategory.AMBIENT, (float) 1, (float) 0.9, false);
+										}
+										if (entity instanceof PlayerEntity) {
+											ItemStack _stktoremove = (itemstack);
+											((PlayerEntity) entity).inventory.func_234564_a_(p -> _stktoremove.getItem() == p.getItem(), (int) 1,
+													((PlayerEntity) entity).container.func_234641_j_());
+										}
+										(itemstack).getOrCreateTag().putBoolean("stopMining", (true));
+									}
+								}
+							}
+						}
+						posZ = (double) (posZ + 1);
+					}
+					posZ = (double) (z - 1);
+					posX = (double) (posX + 1);
+				}
+				posX = (double) (x - 1);
+				posZ = (double) (z - 1);
+				posY = (double) (posY + 1);
 			}
 		}
 	}
